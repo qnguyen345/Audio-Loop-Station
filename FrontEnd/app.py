435792fda1bb61adf3185d861f4a9def7eb7639f
Quyen Nguyen <nguyquye@oregonstate.edu>
2025-01-23 16:22:03 -0800

    lastest UI push

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -9,63 +9,240 @@ app = dash.Dash(__name__)
 
 # Contains the UI layout
 layout = html.Div(
-    style = {"display": "flex", "flexDirection": "column"},
-    children = [
-        
-        
+    className="app-container",
+    children=[
+
+
         # Links css style file
         html.Link(rel="stylesheet", href="./assets/main_style.css"),
-        
-        
+        html.Link(rel="stylesheet", href="./assets/top_style.css"),
+        html.Link(rel="stylesheet", href="./assets/bottom_right_style.css"),
+        html.Link(rel="stylesheet", href="./assets/bottom_left_style.css"),
+        # Link font awesome file to use icons
+        html.Link(
+            rel="stylesheet",
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css",
+        ),
+
+
         # Top section
         html.Div(
             className="top-container",
             children=[
-                
+
                 # Tabs section
                 html.Div(
-                    className="tabs-container",
+                    className="files-container",
                     children=[
-                        ]
-                    ),
-                
+                        html.Button(
+                            className="files-button",
+                            id="files_button",
+                            children="Files",
+                        ),
+                    ]
+                ),
+
                 # App Title
                 html.Div(
-                    className="app-container",
+                    className="title-container",
                     children=[
-                        html.H4(
-                            "Audio Loop Station",
-                            className="title"
-                            )
-                        ]
-                    ),
-                
+                        html.H2(
+                            className="title",
+                            children="Ostinato Live",
+                        )
+                    ]
+                ),
+
                 # Class; members
                 html.Div(
                     className="members-container",
                     children=[
-                        ]
-                    )
-                ]
-            ),
-        
-        
-        # Right section
+                        html.H5(
+                            "CS 467 | Winter 2025 ©",
+                            style={"padding": "2px",
+                                   "margin": "0px"}
+                        ),
+                        html.H5(
+                            "Arthur Tripp | Quyen Nguyen | Matthew Pennington",
+                            style={"padding": "0px",
+                                   "margin": "0px"}
+                        ),
+                    ]
+                )
+            ]
+        ),
+
+
+        # Bottom section
         html.Div(
-            className="right-container"
-            ),
-        
-        
-        # Left section
-        html.Div(
-            className="left-container"
-            ),
-        
-        ]
-    )
+            className="bottom-container",
+            children=[
+                # Left section
+                html.Div(
+                    className="left-container",
+                    children=[
+                    ]
+                ),
+
+                # Right section
+                html.Div(
+                    className="right-container",
+                    children=[
+
+                        # First row of right section
+                        # Contains record, pause and stop buttons
+                        html.Div(
+                            className="right-first-second-row-container",
+                            children=[
+
+                                # Record Button and text
+                                html.Button(
+                                    className="record-button",
+                                    id="record_button",
+                                    children=[
+                                        html.I(
+                                            className="fa-solid fa-microphone"),
+                                        html.Span(children="Record",
+                                                  className="record-text")
+                                    ]
+                                ),
+
+                                # Play Button and Text
+                                html.Button(
+                                    className="play-button",
+                                    id="play_button",
+                                    children=[
+                                        html.I(className="fa-solid fa-play"),
+                                        html.Span(children="Play",
+                                                  className="play-text")
+                                    ]
+                                ),
+
+                                # Pause Button and Text
+                                html.Button(
+                                    className="pause-button",
+                                    id="pause_button",
+                                    children=[
+                                        html.I(className="fa-solid fa-pause"),
+                                        html.Span(className="pause-text",
+                                                  children="Pause")
+                                    ]
+                                ),
+
+                                # Stop Button and Text
+                                html.Button(
+                                    className="stop-button",
+                                    id="stop_button",
+                                    children=[
+                                        html.I(className="fa-solid fa-stop"),
+                                        html.Span(children="Stop",
+                                                  className="stop-text")
+                                    ]
+                                ),
+
+                            ]
+                        ),
+
+                        # Second row of right section
+                        # Contains tempo selection, requirement buttons and
+                        # save button
+                        html.Div(
+                            className="right-third-row-container",
+                            children=[
+
+                                # Tempo selection with - + settings
+                                html.Div(
+                                    className="tempo-container",
+                                    id="tempo_container",
+                                    children=[
+                                        html.Div(
+                                            children=[
+                                                html.Span(className="tempo-text",
+                                                          children="Tempo"),
+                                            ]
+                                        ),
+
+                                        html.Div(
+                                            children=[
+                                                html.Button(
+                                                    className="tempo-button",
+                                                    id="tempo-",
+                                                    children="-"
+                                                ),
+                                                dcc.Input(
+                                                    className="tempo-input",
+                                                    id="tempo_input",
+                                                    type="number",
+                                                    value=100, step=None
+                                                ),
+                                                html.Button(
+                                                    className="tempo-button",
+                                                    id="tempo+",
+                                                    children="+"
+                                                ),
+                                            ]
+                                        )
+                                    ]
+                                ),
+                            ]
+                        ),
+                        
+                        # 3 Requirements Buttons
+                        html.Div(
+                            className="right-fourth-row-container",
+                            children=[
+
+                                # Auto-trimming button
+                                html.Button(
+                                    className="auto-trim-button",
+                                    id="auto_trim_button",
+                                    children="Auto-Trim"
+                                    ),
+                                
+                                # TO BE WORKED ON
+                                html.Button(
+                                    className="auto-trim-button",
+                                    children="Req.2 Button"
+                                    ),
+                                html.Button(
+                                    className="auto-trim-button",
+                                    children="Req.3 Button"
+                                    )
+                            ]
+                        ),
+
+
+                        html.Div(
+                            className="right-fifth-row-container",
+                            children=[
+
+                                # Save button to save loop
+                                html.Button(
+                                    className="save-button",
+                                    id="save_button",
+                                    children=[
+                                        html.Span(className="save-text",
+                                                  children="Save Loop"
+                                                  )
+                                    ]
+                                ),
+
+                            ]
+                        ),
+
+
+                    ]
+                )
+            ]
+        )
+    ]
+)
 
 # Add layout to app
-app.layout = layout
+app.layout= layout
+
+# Get callbacks
+callbacks.button_callbacks(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- FrontEnd/callbacks.py
+++ FrontEnd/callbacks.py
@@ -1,1 +1,66 @@
+import dash
+from dash import dcc, html
 from dash.dependencies import Input, Output, State
+
+
+def button_callbacks(app):
+    """Callbacks for button animations and interactions."""
+    
+    @app.callback(
+        Output("record_button", "className"),
+        Input("record_button", "n_clicks"),
+        prevent_initial_call=True
+    )
+    def record_pulse(n_clicks):
+        """
+        Makes the record buttons pulsing red to indicate recording.
+        Parameters:
+        -----------
+            n-clicks (int or None): number of button clicks. Initially None.
+        Returns:
+        -------
+            button className: contains the button className for css styling
+        """
+        if n_clicks is None or n_clicks % 2 == 0:
+            # Pulsing for clicked button
+            return "record-button"
+        else:
+            return "record-button-pulsing pulse"
+        
+    @app.callback(
+        Output("tempo_input", "value"),
+        [Input("tempo-", "n_clicks"),
+         Input("tempo+", "n_clicks"),
+         Input("tempo_input", "value")],
+        prevent_initial_call=True
+    )
+    def set_tempo(decrease_tempo, increase_tempo, set_tempo):
+        """
+        Sets the tempo if a user adds an input or if the user uses the "-" or
+        "+" button to decrease or increase the tempo.
+        Parameters:
+        -----------
+            decrease_tempo (int or None): number of "-" button clicks.
+                Initially None.
+            increase_tempo (int or None): number of "+" button clicks.
+                Initially None.
+            set_tempo (int): Numeric tempo value that is inputted.
+        Returns:
+        -------
+            set_tempo (int) Numeric tempo value that is going to be set.
+        """
+        
+        # if buttons not clicked, retain current information
+        if not dash.callback_context.triggered:
+            return set_tempo
+        
+        # Get id of button clicked
+        button_id = dash.callback_context.triggered[0]["prop_id"].split(".")[0]
+        
+        # If button is +, add 1, else if button is -, subtract 1
+        if button_id == "tempo-":
+            set_tempo -= 1
+        elif button_id == "tempo+":
+            set_tempo += 1
+            
+        return set_tempo
